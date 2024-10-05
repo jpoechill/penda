57752c07ffec842fa3464870e014d4cd7b374d84
jpoechill <poamrong@rith.com>
2024-10-04 22:26:21 -0700

    refinements

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,11 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-row w-full max-w-[1040px]">
-            <div className="w-full overflow-hidden min-h-[350px] grid md:grid-cols-8 grid-cols-1">
-              <div className="col-span-4 p-5 pl-0">
-                <Image src="/img/portrait_01.jpeg" alt="Group Photo" className="h-[300px] rounded-xl lg:h-full w-full index-0 object-cover bg-cover bg-no-repeat" priority width={1200} height={500}></Image>
+            <div className="w-full overflow-hidden  grid md:grid-cols-6 grid-cols-1">
+              <div className="col-span-1 p-5 pl-0">
+                <Image src="/img/nurse_03.jpeg" alt="Group Photo" className="rounded-xl lg:h-full w-full index-0 object-cover bg-cover bg-no-repeat" priority width={1200} height={500}></Image>
               </div>
-              <div className="col-span-4 p-5">
+              <div className="col-span-5 p-5">
                 At Penda, our mission is to provide compassionate, personalized care that empowers individuals to live independently and with dignity in the comfort of their own homes.
                 <br /><br />
                 We are dedicated to creating a safe, nurturing environment where each client’s physical, emotional, and social needs are met with respect and kindness. Our holistic approach to care ensures that every service is tailored to the unique preferences and requirements of our clients, fostering their well-being and happiness.
