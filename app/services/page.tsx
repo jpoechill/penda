7d5c00888328dc1d73751b0dbd562bff0478cbd7
jpoechill <poamrong@rith.com>
2024-10-21 18:55:17 -0700

    rotation

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
             </div>
             <div className="bg-[#016FA2] rounded-xl p-5 text-white">
               <div className="flex relative flex-row w-full rounded-xl overflow-hidden mb-4">
-                <Image src="/img/cook.jpeg" alt="Group Photo" className="h-[300px] lg:h-full w-full index-0 object-cover bg-cover bg-no-repeat" priority width={1200} height={500}></Image>
+                <Image src="/img/cook.jpg" alt="Group Photo" className="h-[300px] lg:h-full w-full index-0 object-cover bg-cover bg-no-repeat" priority width={1200} height={500}></Image>
               </div>
               <div className={`${bebas.className} text-white text-[24px] pb-2`}>
                 {/* Memory Care */}
